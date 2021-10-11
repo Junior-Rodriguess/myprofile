1e1b3e78987fd9577306cfeca2d974d953271430
Junior-Rodrigues <juniorpsicorodrigues@gmail.com>
2021-10-11 17:49:23 -0300

    feat: create bases progects

---
+++ portifolio/src/App.js
@@ -0,0 +1,62 @@
+import './style.css'
+
+
+function App() {
+  return (
+    <div className="body">
+      <div className="conteiner">
+      <div className="personal-info">
+        <div className="info">
+          <div className="avatar">
+            <img src="https://foxtradeclub.com/wp-content/uploads/2021/06/WhatsApp-Image-2021-06-09-at-17.56.17.jpeg"></img>
+          </div>
+          <h1>Júnior Rodrigues</h1>
+          <p>Developer</p>
+          <span>Florianópolis-SC</span>
+         
+          
+
+          <h2>dashboard</h2>
+
+
+
+
+          
+
+
+
+
+
+
+        </div>
+       
+    
+
+
+
+
+
+
+
+
+
+
+       
+        </div> 
+        <div className="dashboard">
+
+
+
+        </div>
+        
+        
+        
+        
+       </div>
+      
+        
+    </div>
+  );
+}
+
+export default App;
