166eb0d2e60138a1334d362e7b09bb2c280ac2a8
Junior-Rodrigues <juniorpsicorodrigues@gmail.com>
2021-10-11 21:35:27 -0300

    feact:implementiaton the profile info

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,57 +1,50 @@
 import './style.css'
+import { ImLinkedin } from "react-icons/im";
+import { FaGithub } from 'react-icons/fa'; 
+
 
 
 function App() {
   return (
     <div className="body">
-      <div className="conteiner">
+      <div className="container">
       <div className="personal-info">
+      
         <div className="info">
           <div className="avatar">
             <img src="https://foxtradeclub.com/wp-content/uploads/2021/06/WhatsApp-Image-2021-06-09-at-17.56.17.jpeg"></img>
           </div>
           <h1>Júnior Rodrigues</h1>
-          <p>Developer</p>
-          <span>Florianópolis-SC</span>
-         
-          
-
-          <h2>dashboard</h2>
-
-
-
-
-          
-
-
-
-
-
-
+          <p className="label-info">Developer</p>
+          <p className="label-info">Florianópolis-SC</p>
         </div>
-       
-    
 
 
 
+        <div className="widget-contact">
+      
+      <ImLinkedin className="icon"/>
+      <span className="label">Linkedin</span>
+   
+    </div>
+      
 
 
+      
+      <div className="widget-contact">
+      <a href="https://github.com/Junior-Rodriguess">
+      <FaGithub className="icon"/>
+      <span className="label">Github</span>
+      </a>
+      </div>
 
-
-
-
-
-       
         </div> 
-        <div className="dashboard">
+        
+   
 
 
 
-        </div>
-        
-        
-        
-        
+  
        </div>
       
         
